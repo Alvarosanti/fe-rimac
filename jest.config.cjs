c8ca2666266d6f3123f0478565c9ecad9b2390bd
Alvaro Santisteban <53952479+Alvarosanti@users.noreply.github.com>
2025-11-09 14:49:44 +0000

    absolute paths for imports

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,9 +3,14 @@ const { createDefaultPreset } = require("ts-jest");
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
-module.exports =  {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
+module.exports = {
+   preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json', useESM: true }],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
 };
