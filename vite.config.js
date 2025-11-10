2606ad126e3d45e74c9fce14f63e9dd50d83759a
Alvaro Santisteban <alvarosantisteban56@gmail.com>
2025-11-10 13:21:03 -0500

    chore: ignore generated build files

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+        @import "@/styles/_variables.scss";
+        @import "@/styles/_mixins.scss";
+      `,
+            },
+        },
+    },
+});
